e73861ab97bd94f5c021399680ec8cf8f06fba60
ryo8998 <ishino.r8998@gmail.com>
2022-08-23 23:38:12 +0900

    8/23 commit

--- ABC132_D_G_blue_and_red_balls.cpp
+++ ABC132_D_G_blue_and_red_balls.cpp
@@ -1,10 +1,39 @@
 #include <bits/stdc++.h>
-typedef long long ll;
+#include <atcoder/modint>
 using namespace std;
+using namespace atcoder;
+
+using mint = modint1000000007;
+
+typedef long long ll;
+
 #define mod 1000000007
-constexpr array<int, 4> DY = {1, 0, -1, 0};
-constexpr array<int, 4> DX = {0, 1, 0, -1};
+
+mint comb[4005][4005] = {0};
+
+void init(){
+    comb[0][0] = 1;
+    for(int i=0; i<4000; i++){
+        for(int j=0; j<=i; j++){
+            comb[i+1][j] += comb[i][j]; //配るDP
+            comb[i+1][j+1] += comb[i][j]; //配るDP
+        }
+    }
+}
+
+mint nCr(int n, int r){
+  return comb[n][r]; 
+}
 
 int main(){
+    int N,K;
+    cin >> N >> K;
+    init();
+    for(int i=1; i<=K; i++){
+      mint a = comb[K-1][i-1];
+      mint b = comb[N-K+1][i];
+      mint ans = a * b; 
+      cout << ans.val() << endl;
+    }
     
 }
