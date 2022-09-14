9f3724d592307d890363041a5fec91ff27c956c0
ryo8998 <ishino.r8998@gmail.com>
2022-09-14 12:23:57 +0900

    9/14 commit

--- ABC_156_D_G_Bouquet.cpp
+++ ABC_156_D_G_Bouquet.cpp
@@ -4,42 +4,46 @@
 
 using namespace std;
 using namespace atcoder;
-using mint = modint1000000007
+using mint = modint1000000007;
 typedef long long ll;
 // using namespace std;
 #define mod 1000000007
 constexpr array<int, 4> DY = {1, 0, -1, 0};
 constexpr array<int, 4> DX = {0, 1, 0, -1};
-long long catche = 586445753;
 
-mint nCr(mint n,mint r){
-    return fact(n) / fact(r) * fact(n-r);
+
+mint fact(ll n, ll a){
+    mint sum = 1;
+    for(int i=0; i<a; i++){
+        sum *=n;
+        n--;
+    }
+    return sum;
 }
 
-mint fact(mint n){
+mint fact(ll a){
     mint sum = 1;
-    if(n < 10e8){
-        for(ll i=1; i<=n; i++){
+    for(int i=1; i<=a; i++){
         sum *= i;
-        }
-        return sum;
-    }else{
-        for(long long i=900000001;i<=n;i++){
-        ans *= i;
-        }
-        return ans;
     }
     return sum;
 }
 
 
+mint nCr(ll n,ll r){
+    return fact(n,r) / fact(r);
+}
+
+
+
 int main(){
-    mint n,a,b;
+    ll n,a,b;
     cin >> n >> a >> b;
     mint x  = 2;
     mint ans = 0;
     cin >> n >> a >> b;
     ans = x.pow(n) - 1 - nCr(n,a) - nCr(n,b);
-    cout << x.pow(n) << endl;
+    // cout << x.pow(n) << endl;
     cout << ans.val() << endl;
 }
+
