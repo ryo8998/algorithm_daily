8a3a2ba98f3def4d02410d106608c3dd2aa1bb9d
ryo8998 <ishino.r8998@gmail.com>
2022-12-07 23:05:51 +0900

    12/7 commit

---
+++ tessoku/Mathmatical/A29_3_Power.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+typedef unsigned long long ll;
+using namespace std;    
+#define mod 1000000007
+constexpr array<int, 4> DY = {1, 0, -1, 0};
+constexpr array<int, 4> DX = {0, 1, 0, -1};
+
+int main(){
+    ll a,b;
+    cin >> a >> b;
+    ll num = 1;
+    while(b){
+        if((b&1) == 1){
+            num *=a;
+            num %= mod;
+        }
+        a*=a;
+        a%=mod;
+        b>>=1;
+        // cout << a << endl;
+        // cout << b << endl;
+        // cout << num << endl;
+        // cout << endl;
+    }
+    cout << num << endl;
+}
---
+++ tessoku/Mathmatical/B28_Fibonacci.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;    
+#define mod 1000000007
+constexpr array<int, 4> DY = {1, 0, -1, 0};
+constexpr array<int, 4> DX = {0, 1, 0, -1};
+
+int main(){
+    ll N;
+    cin >> N;
+    vector<ll> dp(N+1,0);
+    dp[1] = 1;
+    dp[2] = 1;
+    for(int i=3; i<=N; i++){
+        dp[i] = dp[i-1] + dp[i-2];
+        dp[i]%=mod;
+    }
+    cout << dp[N] << endl;
+}
